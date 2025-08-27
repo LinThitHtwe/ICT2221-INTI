2c7851551126721bcd6cb565d9ea684901070f97
LinThitHtwe <linthit2745@gmail.com>
2025-08-27 12:31:08 +0800

    [init] project & [add] week 1

---
+++ src/main/java/org/example/Pay.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Pay {
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+        // Identifier declarations
+        double hours; // Number of hours worked
+        double rate; // Hourly pay rate
+        double pay; // Gross pay
+        // Display prompts and get input.
+        System.out.print("How many hours did you work? ");
+        hours = keyboard.nextDouble();
+        System.out.print("How much are you paid per hour? ");
+        rate = keyboard.nextDouble();
+        // Perform the calculations.
+        if(hours <= 40)
+            pay = hours * rate;
+        else
+            pay = (hours - 40) * (1.5 * rate) + 40 * rate;
+        // Display results.
+        System.out.println("You earned $" + pay);
+    }
+}
